b022e7b04d8ce029a7700af914d5efb46428147e
Sameer <sameersaini8851@gmail.com>
2026-04-17 13:26:28 +0530

    jer

--- backend/app.py
+++ backend/app.py
@@ -19,6 +19,9 @@ from flask import Flask, send_from_directory, request
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
+
+from backend.routes import *
+
 try:
     from backend.db import init_db
     from backend.routes import register_routes
